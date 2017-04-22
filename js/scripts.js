148dc380f9e2aa387191eb401a57c3ad89226d9e
John Doe <john.doe@example.com>
2017-04-22 23:21:27 +0200

    Triangle area

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Wprowadzenie imienia użytkownika
+var name = prompt('Wpisz swoje imię');
+alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+
+// Wprowadzenie danych do obliczeń
+var a = prompt('Wpisz wartość podstawy trójkąta');
+var h = prompt('Wpisz wartość wysokości trójkąta');
+var triangleArea = a*h/2;
+
+// Przedstawienie wyniku obliczeń
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
